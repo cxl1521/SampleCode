7ddc6617ca4e9887663e264706d3fdee469419b5
cxl1521 <32454634+cxl1521@users.noreply.github.com>
2018-04-10 15:20:51 +0800

    Add files via upload

---
+++ Unit021-GameUsingImageButtonAndImageView/app/src/main/java/com/gameusingimagebuttonandimageview/MainActivity.java
@@ -0,0 +1,104 @@
+package com.gameusingimagebuttonandimageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTxtResult;
+    private ImageView mImgViewComPlay;
+    private ImageButton mImgBtnScissors, mImgBtnStone, mImgBtnPaper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImgViewComPlay = (ImageView)findViewById(R.id.imgViewComPlay);
+        mTxtResult = (TextView)findViewById(R.id.txtResult);
+        mImgBtnScissors = (ImageButton)findViewById(R.id.imgBtnScissors);
+        mImgBtnStone = (ImageButton)findViewById(R.id.imgBtnStone);
+        mImgBtnPaper = (ImageButton)findViewById(R.id.imgBtnPaper);
+
+        mImgBtnScissors.setOnClickListener(imgBtnScissorsOnClick);
+        mImgBtnStone.setOnClickListener(imgBtnStoneOnClick);
+        mImgBtnPaper.setOnClickListener(imgBtnPaperOnClick);
+    }
+
+    private View.OnClickListener imgBtnScissorsOnClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            // 決定電腦出拳.
+            int iComPlay = (int)(Math.random()*3 + 1);
+
+            // 1 – 剪刀, 2 – 石頭, 3 – 布.
+            if (iComPlay == 1) {
+                mImgViewComPlay.setImageResource(R.drawable.scissors);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_draw));
+            }
+            else if (iComPlay == 2) {
+                mImgViewComPlay.setImageResource(R.drawable.stone);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_lose));
+            }
+            else {
+                mImgViewComPlay.setImageResource(R.drawable.paper);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_win));
+            }
+        }
+    };
+
+    private View.OnClickListener imgBtnStoneOnClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            // 決定電腦出拳.
+            int iComPlay = (int)(Math.random()*3 + 1);
+
+            // 1 – 剪刀, 2 – 石頭, 3 – 布.
+            if (iComPlay == 1) {
+                mImgViewComPlay.setImageResource(R.drawable.scissors);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_win));
+            }
+            else if (iComPlay == 2) {
+                mImgViewComPlay.setImageResource(R.drawable.stone);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_draw));
+            }
+            else {
+                mImgViewComPlay.setImageResource(R.drawable.paper);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_lose));
+            }
+        }
+    };
+
+    private View.OnClickListener imgBtnPaperOnClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            // 決定電腦出拳.
+            int iComPlay = (int)(Math.random()*3 + 1);
+
+            // 1 – 剪刀, 2 – 石頭, 3 – 布.
+            if (iComPlay == 1) {
+                mImgViewComPlay.setImageResource(R.drawable.scissors);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_lose));
+            }
+            else if (iComPlay == 2) {
+                mImgViewComPlay.setImageResource(R.drawable.stone);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_win));
+            }
+            else {
+                mImgViewComPlay.setImageResource(R.drawable.paper);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_draw));
+            }
+        }
+    };
+}
+
