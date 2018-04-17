79ca381858e0fa558dc227b1667e787c485998e3
cxl1521 <32454634+cxl1521@users.noreply.github.com>
2018-04-17 11:17:19 +0800

    Add files via upload

---
+++ Unit028-GameUsingDynamicFragment/app/src/main/java/com/gameusingdynamicfragment/GameResultFragment.java
@@ -0,0 +1,44 @@
+package com.gameusingdynamicfragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GameResultFragment extends Fragment {
+
+
+    public GameResultFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // 利用inflater物件的inflate()方法取得介面佈局檔，並將最後的結果傳回給系統
+        return inflater.inflate(R.layout.fragment_game_result, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        MainFragment frag = (MainFragment) getFragmentManager()
+                .findFragmentById(R.id.fragMain);
+        frag.mEdtCountSet = (EditText) getActivity().findViewById(R.id.edtCountSet);
+        frag.mEdtCountPlayerWin = (EditText) getActivity()
+                .findViewById(R.id.edtCountPlayerWin);
+        frag.mEdtCountComWin = (EditText) getActivity()
+                .findViewById(R.id.edtCountComWin);
+        frag.mEdtCountDraw = (EditText)getActivity().findViewById(R.id.edtCountDraw);
+    }
+
+}
