9d868eac638f4432d2ab0b554f72d50e46dbf576
cxl1521 <32454634+cxl1521@users.noreply.github.com>
2018-04-30 20:49:07 +0800

    Add files via upload

---
+++ Unit044-IntentCarryingData/app/src/main/java/com/intentcarryingdata/GameResultActivity.java
@@ -0,0 +1,53 @@
+package com.intentcarryingdata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class GameResultActivity extends AppCompatActivity {
+
+    private EditText mEdtCountSet,
+            mEdtCountPlayerWin,
+            mEdtCountComWin,
+            mEdtCountDraw;
+    private Button mBtnBackToGame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_result);
+
+        mEdtCountSet = (EditText)findViewById(R.id.edtCountSet);
+        mEdtCountPlayerWin = (EditText)findViewById(R.id.edtCountPlayerWin);
+        mEdtCountComWin = (EditText)findViewById(R.id.edtCountComWin);
+        mEdtCountDraw = (EditText)findViewById(R.id.edtCountDraw);
+        mBtnBackToGame = (Button)findViewById(R.id.btnBackToGame);
+
+        mBtnBackToGame.setOnClickListener(btnBackToGameOnClick);
+
+        showResult();
+    }
+
+    private View.OnClickListener btnBackToGameOnClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            finish();
+        }
+    };
+
+    private void showResult() {
+        // 從 Bundle 物件中取出資料
+        Bundle bundle = getIntent().getExtras();
+
+        int iCountSet = bundle.getInt("KEY_COUNT_SET");
+        int iCountPlayerWin = bundle.getInt("KEY_COUNT_PLAYER_WIN");
+        int iCountComWin = bundle.getInt("KEY_COUNT_COM_WIN");
+        int iCountDraw = bundle.getInt("KEY_COUNT_DRAW");
+
+        mEdtCountSet.setText(Integer.toString(iCountSet));
+        mEdtCountPlayerWin.setText(Integer.toString(iCountPlayerWin));
+        mEdtCountComWin.setText(Integer.toString(iCountComWin));
+        mEdtCountDraw.setText(Integer.toString(iCountDraw));
+    }
+}
---
+++ Unit044-IntentCarryingData/app/src/main/java/com/intentcarryingdata/MainActivity.java
@@ -0,0 +1,145 @@
+package com.intentcarryingdata;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTxtResult;
+    private ImageView mImgViewComPlay;
+    private ImageButton mImgBtnScissors, mImgBtnStone, mImgBtnPaper;
+
+    // 新增統計遊戲局數和輸贏的變數
+    private int miCountSet = 0,
+            miCountPlayerWin = 0,
+            miCountComWin = 0,
+            miCountDraw = 0;
+
+    private Button mBtnShowResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImgViewComPlay = (ImageView)findViewById(R.id.imgViewComPlay);
+        mTxtResult = (TextView)findViewById(R.id.txtResult);
+        mImgBtnScissors = (ImageButton)findViewById(R.id.imgBtnScissors);
+        mImgBtnStone = (ImageButton)findViewById(R.id.imgBtnStone);
+        mImgBtnPaper = (ImageButton)findViewById(R.id.imgBtnPaper);
+
+        mImgBtnScissors.setOnClickListener(imgBtnScissorsOnClick);
+        mImgBtnStone.setOnClickListener(imgBtnStoneOnClick);
+        mImgBtnPaper.setOnClickListener(imgBtnPaperOnClick);
+
+        mBtnShowResult = (Button)findViewById(R.id.btnShowResult);
+        mBtnShowResult.setOnClickListener(btnShowResultOnClick);
+    }
+
+    private View.OnClickListener imgBtnScissorsOnClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            // Decide computer play.
+            int iComPlay = (int)(Math.random()*3 + 1);
+
+            miCountSet++;
+
+            // 1 - scissors, 2 - stone, 3 - net.
+            if (iComPlay == 1) {
+                mImgViewComPlay.setImageResource(R.drawable.scissors);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_draw));
+                miCountDraw++;
+            }
+            else if (iComPlay == 2) {
+                mImgViewComPlay.setImageResource(R.drawable.stone);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_lose));
+                miCountComWin++;
+            }
+            else {
+                mImgViewComPlay.setImageResource(R.drawable.paper);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_win));
+                miCountPlayerWin++;
+            }
+        }
+    };
+
+    private View.OnClickListener imgBtnStoneOnClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            int iComPlay = (int)(Math.random()*3 + 1);
+
+            miCountSet++;
+
+            // 1 - scissors, 2 - stone, 3 - net.
+            if (iComPlay == 1) {
+                mImgViewComPlay.setImageResource(R.drawable.scissors);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_win));
+                miCountPlayerWin++;
+            }
+            else if (iComPlay == 2) {
+                mImgViewComPlay.setImageResource(R.drawable.stone);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_draw));
+                miCountDraw++;
+            }
+            else {
+                mImgViewComPlay.setImageResource(R.drawable.paper);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_lose));
+                miCountComWin++;
+            }
+        }
+    };
+
+    private View.OnClickListener imgBtnPaperOnClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            int iComPlay = (int)(Math.random()*3 + 1);
+
+            miCountSet++;
+
+            // 1 - scissors, 2 - stone, 3 - net.
+            if (iComPlay == 1) {
+                mImgViewComPlay.setImageResource(R.drawable.scissors);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_lose));
+                miCountComWin++;
+            }
+            else if (iComPlay == 2) {
+                mImgViewComPlay.setImageResource(R.drawable.stone);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_win));
+                miCountPlayerWin++;
+            }
+            else {
+                mImgViewComPlay.setImageResource(R.drawable.paper);
+                mTxtResult.setText(getString(R.string.result) +
+                        getString(R.string.player_draw));
+                miCountDraw++;
+            }
+        }
+    };
+
+    private View.OnClickListener btnShowResultOnClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent it = new Intent();
+            it.setClass(MainActivity.this, GameResultActivity.class);
+
+            Bundle bundle = new Bundle();
+            bundle.putInt("KEY_COUNT_SET", miCountSet);
+            bundle.putInt("KEY_COUNT_PLAYER_WIN", miCountPlayerWin);
+            bundle.putInt("KEY_COUNT_COM_WIN", miCountComWin);
+            bundle.putInt("KEY_COUNT_DRAW", miCountDraw);
+            it.putExtras(bundle);
+
+            startActivity(it);
+        }
+    };
+}
