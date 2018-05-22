0c65ba87806fab1e2bb7b5e6a3dbebd48298fa82
cxl1521 <32454634+cxl1521@users.noreply.github.com>
2018-05-23 07:40:52 +0800

    Add files via upload

---
+++ Unit076-GoogleMapGPSWithLocationListener/app/src/main/java/com/googlemapapp/MapsActivity.java
@@ -0,0 +1,368 @@
+package com.googlemapapp;
+
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MapsActivity extends FragmentActivity
+        implements OnMapReadyCallback,
+                    GoogleApiClient.ConnectionCallbacks,
+                    GoogleApiClient.OnConnectionFailedListener,
+                    LocationListener {
+
+    // 定義這個權限要求的編號
+    private final int REQUEST_PERMISSION_FOR_ACCESS_FINE_LOCATION = 100;
+
+    private GoogleMap mMap;
+
+    private Spinner mSpnLocation;
+    private static String[][] mLocations = {
+            {"台北101", "25.0336110,121.5650000"},
+            {"中國長城", "40.0000350,119.7672800"},
+            {"紐約自由女神像", "40.6892490,-74.0445000"},
+            {"巴黎鐵塔", "48.8582220,2.2945000"}	};
+
+    private SupportMapFragment mSupportMapFragment;
+    private boolean mbIsZoomFirst = true;
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private LocationManager mLocationMgr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+
+        mSpnLocation = (Spinner) this.findViewById(R.id.spnLocation);
+
+        ArrayAdapter<String> arrAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item);
+
+        for (int i = 0; i < mLocations.length; i++)
+            arrAdapter.add(mLocations[i][0]);
+
+        arrAdapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        mSpnLocation.setAdapter(arrAdapter);
+        mSpnLocation.setOnItemSelectedListener(spnLocationOnItemSelected);
+
+        Spinner spnMapType = (Spinner) findViewById(R.id.spnMapType);
+        spnMapType.setOnItemSelectedListener(spnMapTypeOnItemSelected);
+
+        Button btn3DMap = (Button) findViewById(R.id.btn3DMap);
+        btn3DMap.setOnClickListener(btn3DMapOnClick);
+
+        // 建立SupportMapFragment，並且設定Map的callback
+        mSupportMapFragment = new SupportMapFragment();
+        mSupportMapFragment.getMapAsync(this);
+
+        // 把SupportMapFragment放到介面佈局檔裡頭的FrameLayout顯示。
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.frameLayMapContainer, mSupportMapFragment)
+                .commit();
+
+        // 建立一個GoogleApiClient物件。
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mLocationMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // 啟動 Google API。
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        enableLocationAndGetLastLocation(false);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // 停用 Google API
+        Toast.makeText(MapsActivity.this, "停用Google API", Toast.LENGTH_LONG)
+                .show();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // 檢查收到的權限要求編號是否和我們送出的相同
+        if (requestCode == REQUEST_PERMISSION_FOR_ACCESS_FINE_LOCATION) {
+            if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 再檢查一次，就會進入同意的狀態，並且順利啟動。
+                Location location = enableLocationAndGetLastLocation(true);
+
+                if (location != null) {
+                    Toast.makeText(MapsActivity.this, "成功取得上一次定位",
+                            Toast.LENGTH_LONG).show();
+                    onLocationChanged(location);    // 更新地圖的定位。
+                } else
+                    Toast.makeText(MapsActivity.this, "沒有上一次定位的資料",
+                            Toast.LENGTH_LONG).show();
+
+                return;
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+
+    private AdapterView.OnItemSelectedListener spnLocationOnItemSelected =
+            new AdapterView.OnItemSelectedListener() {
+
+                @Override
+                public void onItemSelected(AdapterView parent, View v,
+                                           int position, long id) {
+                    updateMapLocation();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView parent) {
+                }
+
+            };
+
+    private void updateMapLocation() {
+        int iSelected = mSpnLocation.getSelectedItemPosition();
+        String[] sLocation = mLocations[iSelected][1].split(",");
+        double dLat = Double.parseDouble(sLocation[0]);	// 南北緯
+        double dLon = Double.parseDouble(sLocation[1]);	// 東西經
+
+        // 如果是第一次執行，把地圖放大到設定的等級。
+        if (mbIsZoomFirst) {
+            mbIsZoomFirst = false;
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(dLat, dLon), 15));
+        } else
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(
+                    new LatLng(dLat, dLon)));
+    }
+
+    private AdapterView.OnItemSelectedListener spnMapTypeOnItemSelected =
+            new AdapterView.OnItemSelectedListener() {
+
+                @Override
+                public void onItemSelected(AdapterView parent,
+                                           View v, int position, long id) {
+                    // 依照使用者點選的項目位置，改變地圖模式。
+                    switch (position) {
+                        case 0:
+                            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                            break;
+                        case 1:
+                            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                            break;
+                        case 2:
+                            mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                            break;
+                        case 3:
+                            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                            break;
+                    }
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView parent) {
+                }
+            };
+
+    private View.OnClickListener btn3DMapOnClick = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            // 設定地圖的俯視角度，並且放大到一定的等級，讓3D建築物出現。
+            CameraUpdate camUpdate = CameraUpdateFactory.newCameraPosition(
+                    new CameraPosition.Builder()
+                            .target(mMap.getCameraPosition().target)
+                            .tilt(60)
+                            .zoom(18)
+                            .build());
+            mMap.animateCamera(camUpdate);
+        }
+    };
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // Google API 連線成功時會執行這個方法。
+        Toast.makeText(MapsActivity.this, "Google API 連線成功",
+                Toast.LENGTH_LONG).show();
+
+        // 啟動定位
+        Location location = enableLocationAndGetLastLocation(true);
+
+        if (location != null) {
+            Toast.makeText(MapsActivity.this, "成功取得上一次定位",
+                    Toast.LENGTH_LONG).show();
+            onLocationChanged(location);    // 更新地圖的定位。
+        } else
+            Toast.makeText(MapsActivity.this, "沒有上一次定位的資料",
+                    Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // Google API 無故斷線時，才會執行這個方法。
+        // 程式呼叫L disconnect()時不會執行這個方法。
+        switch (cause) {
+            case CAUSE_NETWORK_LOST:
+                Toast.makeText(MapsActivity.this, "網路斷線，無法定位",
+                        Toast.LENGTH_LONG).show();
+                break;
+            case CAUSE_SERVICE_DISCONNECTED:
+                Toast.makeText(MapsActivity.this, "Google API 異常，無法定位",
+                        Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // 收到新的定位資料時執行這個方法。
+        // location物件中包含定位的經緯度資料。
+        // 移動地圖到新位置。
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(
+                new LatLng(location.getLatitude(), location.getLongitude())));
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // 和 Google API 連線失敗時會執行這個方法。
+        Toast.makeText(MapsActivity.this, "Google API 連線失敗",
+                Toast.LENGTH_LONG).show();
+    }
+
+    private Location enableLocationAndGetLastLocation(boolean on) {
+        if (ContextCompat.checkSelfPermission(MapsActivity.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // 這項功能尚未取得使用者的同意
+            // 開始執行徵詢使用者的流程
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    MapsActivity.this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                AlertDialog.Builder altDlgBuilder = new AlertDialog.Builder(MapsActivity.this);
+                altDlgBuilder.setTitle("提示");
+                altDlgBuilder.setMessage("App需要啟動定位功能。");
+                altDlgBuilder.setIcon(android.R.drawable.ic_dialog_info);
+                altDlgBuilder.setCancelable(false);
+                altDlgBuilder.setPositiveButton("確定",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // 顯示詢問使用者是否同意功能權限的對話盒
+                                // 使用者答覆後會執行onRequestPermissionsResult()
+                                ActivityCompat.requestPermissions(MapsActivity.this,
+                                        new String[]{
+                                                android.Manifest.permission.ACCESS_FINE_LOCATION},
+                                        REQUEST_PERMISSION_FOR_ACCESS_FINE_LOCATION);
+                            }
+                        });
+                altDlgBuilder.show();
+
+                return null;
+            } else {
+                // 顯示詢問使用者是否同意功能權限的對話盒
+                // 使用者答覆後會執行onRequestPermissionsResult()
+                ActivityCompat.requestPermissions(MapsActivity.this,
+                        new String[]{
+                                android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_PERMISSION_FOR_ACCESS_FINE_LOCATION);
+
+                return null;
+            }
+        }
+
+        // 這項功能之前已經取得使用者的同意，可以直接使用
+        Location lastLocation = null;
+        if (on) {
+            // 取得上一次定位資料
+            lastLocation = LocationServices.FusedLocationApi
+                    .getLastLocation(mGoogleApiClient);
+
+            // 準備一個LocationRequest物件，設定定位參數，在啟動定位時使用
+            mLocationRequest = LocationRequest.create();
+            mLocationRequest.setInterval(5000);    // 二次定位之間的時間間隔，單位是千分之一秒。
+            mLocationRequest.setSmallestDisplacement(5);    // 二次定位之間的最大距離，單位是公尺。
+
+            // 啟動定位，如果GPS功能有開啟，優先使用GPS定位，否則使用網路定位。
+            if (mLocationMgr.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                Toast.makeText(MapsActivity.this, "使用GPS定位", Toast.LENGTH_LONG)
+                        .show();
+            } else if (mLocationMgr.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                mLocationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+                Toast.makeText(MapsActivity.this, "使用網路定位", Toast.LENGTH_LONG)
+                        .show();
+            }
+
+            // 啟動定位功能
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                    mGoogleApiClient, mLocationRequest, this);
+        } else {
+            // 停止定位功能
+            LocationServices.FusedLocationApi.removeLocationUpdates(
+                    mGoogleApiClient, this);
+            Toast.makeText(MapsActivity.this, "停止定位", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+        return lastLocation;
+    }
+}
